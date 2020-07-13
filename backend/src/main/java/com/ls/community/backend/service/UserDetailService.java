c712b3aaee47f90d09e1d792f96d4fa17f3ec110
jeongseop Shim <js0507dev@gmail.com>
2020-07-13 23:45:38 +0900

    oauth2 인증서버 셋팅중

--- backend/src/main/java/com/ls/community/backend/service/UserDetailService.java
+++ backend/src/main/java/com/ls/community/backend/service/UserDetailService.java
@@ -5,9 +5,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailService implements UserDetailsService {
-  private UserRepository userRepository;
+  //private UserRepository userRepository;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return null;
   }
 }
