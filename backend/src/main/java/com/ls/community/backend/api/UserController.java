c50a5ab303f068e7fb3ca3cccc4c7436dec0bff7
jeongseop Shim <js0507dev@gmail.com>
2020-07-19 19:53:49 +0900

    Initialize Project

--- backend/src/main/java/com/ls/community/backend/api/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.ls.community.backend.api;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/user")
-public class UserController {
-}
